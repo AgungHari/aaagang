7d8e8d3b70ac6f426d3be2afa80fd57e4981817a
AgungHari <jerokedaton2@gmail.com>
2026-04-18 23:18:47 +0700

    correct Frame99.webp filename case sensitivity

--- src/components/TiltImage.tsx
+++ src/components/TiltImage.tsx
@@ -53,7 +53,7 @@ export default function TiltedImage({ rotateAmplitude = 3, }) {
             transition={{ type: "spring", stiffness: 320, damping: 70, mass: 1 }}
         >
             <motion.div className="relative transform-3d w-full max-w-4xl" style={{ rotateX, rotateY }} >
-                <motion.img src="/frame99.webp"
+                <motion.img src="/Frame99.webp"
                     className="w-full rounded-[15px] will-change-transform transform-[translateZ(0)]"
                     alt="AAA Gang"
                 />
